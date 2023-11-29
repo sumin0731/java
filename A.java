95f7322196f3682e2268dfb33edea5a861ebf40f
Su Min Lee <127282598+sumin0731@users.noreply.github.com>
2023-11-29 12:10:51 +0900

    1129
    바깥 멤버 접근

--- A.java
+++ A.java
@@ -1,23 +1,5 @@
-package ch09.sec03.exam02;
+package ch09.sec05.exam02;
 
 public class A {
-	
-	static class B {
-		int field1 = 1;
-		
-		static int field2 = 2;
-		
-		B() {
-				System.out.println("B-생성자 실행");
-		}
-		
-		void method1() {
-			System.out.println("B-method1 실행");
-		}
-		
-		static void method2() {
-			System.out.println("B-method2 실행");
-		}
-	}
 
 }
--- AExample.java
+++ AExample.java
@@ -1,15 +1,9 @@
-package ch09.sec03.exam02;
+package ch09.sec05.exam02;
 
 public class AExample {
 
 	public static void main(String[] args) {
-
-		A.B b = new A.B();
-		System.out.println(b.field1);
-		b.method1();
-		
-		System.out.println(A.B.field2);
-		A.B.method2();
+		// TODO Auto-generated method stub
 
 	}
 
