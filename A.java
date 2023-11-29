45c6c0949950846d90e0d7367026052042be5767
Su Min Lee <127282598+sumin0731@users.noreply.github.com>
2023-11-29 11:45:00 +0900

    1129
    정적 멤버 클래스

--- A.java
+++ A.java
@@ -1,8 +1,8 @@
-package ch09.sec02.exam02;
+package ch09.sec03.exam02;
 
 public class A {
 	
-	class B {
+	static class B {
 		int field1 = 1;
 		
 		static int field2 = 2;
@@ -12,21 +12,12 @@ public class A {
 		}
 		
 		void method1() {
-				System.out.println("B-mothod1 실행");
+			System.out.println("B-method1 실행");
 		}
 		
 		static void method2() {
-				System.out.println("B-method2 실행");
+			System.out.println("B-method2 실행");
 		}
-	}
-	
-	void useB() {
-		B b = new B();
-		System.out.println(b.field1);
-		b.method1();
-		
-		System.out.println(B.field2);
-		B.method2();
 	}
 
 }
--- AExample.java
+++ AExample.java
@@ -1,12 +1,15 @@
-package ch09.sec02.exam02;
+package ch09.sec03.exam02;
 
 public class AExample {
 
 	public static void main(String[] args) {
 
-		A a = new A();
+		A.B b = new A.B();
+		System.out.println(b.field1);
+		b.method1();
 		
-		a.useB();
+		System.out.println(A.B.field2);
+		A.B.method2();
 
 	}
 
